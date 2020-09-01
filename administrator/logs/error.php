b7472689bad068a896430f4989cba0834fae38fc
PadmaGnanapriya <padmaisuru@gmail.com>
2020-09-01 06:47:56 +0530

    first commit

---
+++ administrator/logs/error.php
@@ -0,0 +1,9 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2020-08-29 23:57:44 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2020-08-29T23:57:44+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2020-08-29T23:57:49+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2020-08-29T23:57:56+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
